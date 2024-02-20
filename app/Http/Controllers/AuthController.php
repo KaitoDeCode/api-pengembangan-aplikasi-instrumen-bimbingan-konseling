798a7582a89dc7004045b5baf834067a72d4f9ce
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-20 18:38:01 +0700

    feat:  register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RequestLogin;
 use App\Http\Requests\RequestRegister;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
+    public function login(RequestLogin $request){
 
     }
 
     public function register(RequestRegister $request){
-
+        $data = $request->validated();
+        $user = User::create([
+            "name" => $data["name"],
+            "email"=> $data["email"],
+            "password"=> bcrypt($data["password"]),
+        ]);
+        return response()->json([
+            "success"=> true,
+            "message" => "success register",
+        ], 200);
     }
 }
