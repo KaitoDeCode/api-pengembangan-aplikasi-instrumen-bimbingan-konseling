132f0d43a83a0a3df351363d8c1f88120d1b17f3
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-20 18:43:00 +0700

    feat: login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,11 +6,20 @@ use App\Http\Requests\RequestLogin;
 use App\Http\Requests\RequestRegister;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function login(RequestLogin $request){
-
+        $data = $request->validated();
+        $user = User::where('email',$data['email'])->first();
+        if($user && Hash::check($data['password'],$user->password)){
+            return response()->json([
+                "success"=> false,
+                "message" => "invalid credentials",
+            ], 404);
+        }
+        $token = $user->createToken('access-token')->plainTextToken;
     }
 
     public function register(RequestRegister $request){
